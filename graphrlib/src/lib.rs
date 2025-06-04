d2cb2b235d7536b18868b2f565c0bbaece16a020
Jokubas Pelanskis at Cottrell <jp773@cam.ac.uk>
2025-06-04 21:55:59 +0100

    bakcup

--- graphrlib/src/lib.rs
+++ graphrlib/src/lib.rs
@@ -6,13 +6,15 @@ DNode - Only contains abstract calculations. real data are only described by loc
 use std::collections::BTreeMap;
 use pyo3::prelude::*;
 use serde::{Serialize, Deserialize};
+use regex::Regex;
 
 /// Set types for interacting with the database
 type IdCTemplate = String;
 type IdDTemplate = String;
-type IdC = u32;
-type IdD = u32;
+type IdC = u128;
+type IdD = u128;
 type IdTemplate = String;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 
 /// Describe Abstract Data and Calculation nodes
@@ -28,8 +30,8 @@ pub struct DNodeTemplate {
 pub struct CNodeTemplate {
     pub id: IdCTemplate,
     pub command: String,
-    pub incoming: Vec<DNodeTemplate>,
-    pub outcoming: Vec<DNodeTemplate>
+    pub incoming: Vec<IdDTemplate>,
+    pub outcoming: Vec<IdDTemplate>
 }
 
 #[pyclass]
@@ -42,10 +44,11 @@ enum NodeTemplate {
 
 /// Describes abstract calculations.
 #[pyclass]
+#[derive(Clone)]
 pub struct DatabaseTemplate {
     id: IdTemplate,
-    CNodes: BTreeMap<IdCTemplate, CNodeTemplate>, // Store all calculation nodes
-    DNodes: BTreeMap<IdDTemplate, DNodeTemplate>, // Store all data nodes
+    cnodes: BTreeMap<IdCTemplate, CNodeTemplate>, // Store all calculation nodes
+    dnodes: BTreeMap<IdDTemplate, DNodeTemplate>, // Store all data nodes
 }
 
 
@@ -65,8 +68,8 @@ pub struct CNode{
     pub id: IdC,
     pub template: IdCTemplate,
     pub command: String,
-    pub incoming: Vec<DNode>,
-    pub outcoming: Vec<DNode>
+    pub incoming: Vec<IdD>,
+    pub outcoming: Vec<IdD>
 }
 
 #[pyclass]
@@ -77,14 +80,93 @@ enum Node {
 }
 
 
-
 #[pymethods]
-impl DNodeTemplate {
-    fn __str__(&self) -> String {
-        format!("'id': {}", self.id)
+impl Node {
+
+    /// Generate an id for a calculation node.
+    #[staticmethod]
+    fn generate_id() -> IdC{
+
+        let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Failed to get current system time.")
+        .as_nanos()
+        .try_into().expect("value out of range");
+
+        now
     }
 }
 
+
+
+#[pymethods]
+impl DNodeTemplate {
+    fn __str__(&self) -> PyResult<String> {
+        Ok(format!("DNodeTemplate(id={})", self.id))
+    }
+
+    /// Format the string so that it could be interpreted by calculation creation 
+    /// algorithm
+    fn __format__(&self, spec: &str) -> PyResult<String> {
+        let formatted = match spec {
+            _ => format!("{}", self.id),
+        };
+        Ok(formatted)
+    }
+}
+
+
+#[pymethods]
+impl CNodeTemplate {
+    fn __str__(&self) -> PyResult<String> {
+        Ok(format!("DNodeTemplate(id={};\ncommand={},\ninput={:?},\noutput={:?})", self.id, self.command,self.incoming,self.outcoming ))
+    }
+
+}
+
+
+
+
+impl CNodeTemplate {
+
+    /// Parses a command to the desirable format
+    /// command has inputs marked with input(<filename>) and output with output(<filename>)
+    fn parse_command(command: String) -> (String, Vec<IdDTemplate>, Vec<IdDTemplate>) {
+        
+        let re = Regex::new(r"(input|output)\(([^)]+)\)").expect("Failed to compile input regex.");
+
+        let mut inputs = Vec::new();
+        let mut outputs = Vec::new();
+        let mut input_counter = 0;
+        let mut output_counter = 0;
+        // Replace input(<filename>) with $i, where i is an integer enumerating all input
+
+        let output = re.replace_all(&command, |caps: &regex::Captures| {
+            let kind = &caps[1];     // "input" or "output"
+            let value = &caps[2];    // the part inside parentheses
+    
+            match kind {
+                "input" => {
+                    inputs.push(value.to_string());
+                    let replacement = format!("$i_{}", input_counter);
+                    input_counter +=1;
+                    replacement
+                    },
+                "output" => {
+                    outputs.push(value.to_string());
+                    let replacement = format!("$o_{}", output_counter);
+                    output_counter +=1;
+                    replacement
+
+                },
+                _ => {panic!("Could not parse the command correctly")}
+            }
+        });
+
+        (output.to_string(), inputs, outputs)
+
+    }
+}
 
 
 #[pymethods]
@@ -94,24 +176,48 @@ impl DatabaseTemplate {
     pub fn new(name: IdCTemplate) -> Self {
         DatabaseTemplate {
             id : name,
-            CNodes: BTreeMap::new(),
-            DNodes: BTreeMap::new(),
+            cnodes: BTreeMap::new(),
+            dnodes: BTreeMap::new(),
         }
 
     }
 
+    fn __str__(&self) -> PyResult<String> {
+
+        let cnodes = format!("{:?}", self.cnodes);
+        let dnodes = format!("{:?}", self.dnodes);
+        Ok(format!("DatabaseTemplate(cnodes={};\ndnodes={})",cnodes, dnodes  ))
+    }
+
+
+
     /// Register data node
     pub fn register_dnode(&mut self, name: String) -> DNodeTemplate {
+
         let dnode = DNodeTemplate {id: name.clone()};
-        self.DNodes.insert(name, dnode.clone());
+        self.dnodes.insert(name, dnode.clone());
         dnode
     }
 
     /// Register calculation node
+    /// If a key already exists, then comprate the value. If the values are the same do nothing,
+    /// If they are different then crash, otherwise, overwrite flag is passed
     pub fn register_cnode(&mut self, name: String, command: String) -> CNodeTemplate {
-        let cnode = CNodeTemplate {id: name.clone()};
-        self.CNodes.insert(name, dnode.clone());
+        
+
+
+        let values = CNodeTemplate::parse_command(command);
+
+        let cnode = CNodeTemplate {
+            id: name.clone(),
+            incoming: values.1,
+            outcoming: values.2,
+            command: values.0,
+        };
+
+        self.cnodes.insert(name, cnode.clone());
         cnode
+
     }
 
     /// get a node from a template
@@ -119,9 +225,62 @@ impl DatabaseTemplate {
         unimplemented!();
     }
 
-    /// Create a calculation
+    /// Create an implementation of a given template.
     pub fn create_calculation(&self, leafs: Vec<(String,String)>) -> Database {
-        unimplemented!();
+        
+        // Need to go through all data and calculation nodes and generate actual calculations.
+
+        // Go through all Data nodes
+
+        let mut new_dnodes  = BTreeMap::new();
+        let mut new_cnodes = BTreeMap::new();
+        let mut dnode_mapping = BTreeMap::new(); // for fully mapping data nodes
+        
+        // data nodes
+        for (key, value) in &self.dnodes {
+            // Create the node
+            let cid = Node::generate_id();
+            let dnode = DNode {
+                id: cid,
+                template: value.id.clone(),
+            };
+            // insert into the final
+            new_dnodes.insert(cid, dnode);
+            // insert into the remaping
+            dnode_mapping.insert(key, cid);
+
+        }
+
+        // Create all the new data nodes
+        for (key, value) in &self.cnodes {
+            let cid = Node::generate_id();
+
+            let map_with_error = |k: &String| {
+                dnode_mapping.get(k).cloned().unwrap_or_else(|| {
+                    panic!("Failed to find '{}' in input mappings. Aborting.", k);
+                })
+            };
+        
+            let cnode = CNode {
+                id: cid,
+                template: value.id.clone(),
+                command: value.command.clone(),
+                incoming: value.incoming.iter().map(map_with_error).collect(),
+                outcoming: value.outcoming.iter().map(map_with_error).collect(),
+            };
+        
+            new_cnodes.insert(cid, cnode);
+        }
+
+
+
+        // Generate a database
+        let db = Database {
+            template: self.clone(),
+            cnodes : new_cnodes,
+            dnodes : new_dnodes,
+            };
+        db
     }
 
 }
@@ -132,14 +291,23 @@ impl DatabaseTemplate {
 pub struct Database {
 
     template: DatabaseTemplate, // Store the template
-    CNodes: BTreeMap<IdC, CNode>, // Store all calculation nodes
-    DNodes: BTreeMap<IdD, DNode>, // Store all data nodes
+    cnodes: BTreeMap<IdC, CNode>, // Store all calculation nodes
+    dnodes: BTreeMap<IdD, DNode>, // Store all data nodes
 
 }
 
 /// Implement all selection and filtering functions
 #[pymethods]
 impl Database {
+
+    fn __str__(&self) -> PyResult<String> {
+
+        let cnodes = format!("{:?}", self.cnodes);
+        let dnodes = format!("{:?}", self.dnodes);
+        Ok(format!("DatabaseTemplate(cnodes={};\ndnodes={})",cnodes, dnodes  ))
+    }
+
+
     /// Select all nodes based on name
     pub fn select(&self, name: Node) -> Database {
         unimplemented!();
